7ce2a493f5b5b7e5d1f93c95e947f80a99b55d38
Tamil0142 <42166616+Tamil0142@users.noreply.github.com>
2018-08-22 10:46:08 +0530

    Create StringPrint.java

---
+++ StringPrint.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class wifi{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		String s=in.next();
+		int n=in.nextInt();
+		for(int i=0;i<n;i++){
+			System.out.println(s);
+		}
+}}
